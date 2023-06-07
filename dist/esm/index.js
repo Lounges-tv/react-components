1022fd5c80a718a758116eed410ba1a9eaabe057
Ksenia Koneva <ksenia.koneva@lounges.tv>
2023-06-07 17:06:18 +0100

    build

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,2 +1,2 @@
-import e from"react";function t({className:t,fill:r,stroke:l}){return e.createElement("svg",{className:t,fill:"none",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 12 16"},e.createElement("path",{d:"M5.4 10.2 1 14V2c0-.6.4-1 1-1h8c.6 0 1 .4 1 1v11.9l-4.4-3.7-.6-.5-.6.5Z",fill:r||"transparent",stroke:l||"#fff","stroke-width":"2"}))}export{t as ShortlistIcon};
+import e from"react";function t(t){var r=t.className,l=t.fill,a=t.stroke;return e.createElement("svg",{className:r,fill:"none",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 12 16"},e.createElement("path",{d:"M5.4 10.2 1 14V2c0-.6.4-1 1-1h8c.6 0 1 .4 1 1v11.9l-4.4-3.7-.6-.5-.6.5Z",fill:l||"transparent",stroke:a||"#fff","stroke-width":"2"}))}export{t as ShortlistIcon};
 //# sourceMappingURL=index.js.map
