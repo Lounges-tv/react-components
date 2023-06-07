e2605d3201f59ee260d7efd7dd79a38753cc9ea4
Ksenia Koneva <ksenia.koneva@lounges.tv>
2023-06-07 15:54:48 +0100

    feat(LE-439): build

---
+++ dist/cjs/index.js
@@ -0,0 +1,2 @@
+"use strict";var t=require("react");exports.ShortlistIcon=function({className:e}){return t.createElement("svg",{className:e,viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg","data-testid":"ShortlistIcon"},t.createElement("path",{d:"M5 3H19C20.6569 3 22 4.34315 22 6V20.25L12 17L2 20.25V6C2 4.34315 3.34315 3 5 3Z",stroke:"black","stroke-width":"2",fill:"evenodd"}))};
+//# sourceMappingURL=index.js.map
---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+declare function ShortlistIcon({ className }: {
+    className?: string;
+}): React.JSX.Element;
+
+export { ShortlistIcon };
